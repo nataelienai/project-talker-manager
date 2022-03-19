9caced46208a8a4ff576ac9c714700b4c9fa6763
Natã Elienai <nata.elienai@gmail.com>
2022-03-19 18:55:43 -0300

    refactor: create the talker controller

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -1,18 +1,8 @@
 const express = require('express');
-const fs = require('fs/promises');
-
-const TALKERS_FILE_PATH = './talker.json';
+const { getTalkers } = require('../controllers/talker');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(TALKERS_FILE_PATH, 'utf-8');
-    const talkers = JSON.parse(fileContent);
-    res.status(200).json(talkers);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.get('/', getTalkers);
 
 module.exports = router;
