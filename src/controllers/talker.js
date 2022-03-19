70576b04c9994ac35a9b2ab820eaba0f78c8dc36
Natã Elienai <nata.elienai@gmail.com>
2022-03-19 19:57:03 -0300

    refactor: change exports to module.exports when exporting

--- src/controllers/talker.js
+++ src/controllers/talker.js
@@ -3,9 +3,11 @@ const rescue = require('express-rescue');
 
 const TALKER_FILE_PATH = './talker.json';
 
-exports.getTalkers = rescue(async (_req, res) => {
+const getTalkers = rescue(async (_req, res) => {
   const fileContent = await fs.readFile(TALKER_FILE_PATH, 'utf-8');
   const talkers = JSON.parse(fileContent);
 
   res.status(200).json(talkers);
 });
+
+module.exports = { getTalkers };
