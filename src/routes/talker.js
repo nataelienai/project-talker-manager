79eff6eca9186e2afda6fdaeeb9e1e5d986d2c0d
Natã Elienai <nata.elienai@gmail.com>
2022-03-19 18:45:26 -0300

    feat: create the GET /talker endpoint

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const fs = require('fs/promises');
+
+const TALKERS_FILE_PATH = './talker.json';
 
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(TALKERS_FILE_PATH, 'utf-8');
+    const talkers = JSON.parse(fileContent);
+    res.status(200).json(talkers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
