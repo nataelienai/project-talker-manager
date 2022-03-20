b78eb261a6837266889e232f08c3c11abb3b88cf
Natã Elienai <nata.elienai@gmail.com>
2022-03-19 21:30:21 -0300

    feat: change error object property name

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 function errorHandler(err, _req, res, _next) {
-  res.status(err.status || 500).json({ error: err.message });
+  res.status(err.status || 500).json({ message: err.message });
 }
 
 module.exports = errorHandler;
