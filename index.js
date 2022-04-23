a45516c9742e7fd96035c6edd0d898b54fd52aab
Natã Elienai <nata.elienai@gmail.com>
2022-04-23 17:29:35 -0300

    chore: remove unnecessary files owned by Trybe

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const loginRouter = require('./src/routes/login');
 const app = express();
 app.use(express.json());
 
-const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
 
 app.use('/talker', talkerRouter);
 
